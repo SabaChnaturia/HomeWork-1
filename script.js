11c3e34afdd891bcc210bc7d55e77a9077646247
SabaChnaturia <154993776+SabaChnaturia@users.noreply.github.com>
2024-07-23 15:27:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Name is : Mindia");
+console.log("Surname is : Arabuli");
+console.log("Age : 100");
+console.warn("Email : Test@gmail.com");
+console.warn("Password : 123456");
+console.log("Phone : 555-555-555");
